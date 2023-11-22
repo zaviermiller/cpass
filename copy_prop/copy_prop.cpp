6d02fd3e1d21d586b511f3bf79f7502788da609f
Zavier Miller <zavierjmiller@gmail.com>
2023-11-21 23:01:25 -0500

    remove debug stuff

--- copy_prop/copy_prop.cpp
+++ copy_prop/copy_prop.cpp
@@ -130,8 +130,8 @@ void CopyPropagation::propagateCopies(BasicBlock &bb, ACPTable &acp)
     if (isa<StoreInst>(iptr)) {
       Value *dest = ins.getOperand(1);
       Value *src = ins.getOperand(0);
-      ins.print(errs());
-      errs() << " src: " << src << " dest: " << dest << "\n";
+      // ins.print(errs());
+      // errs() << " src: " << src << " dest: " << dest << "\n";
       for (i = 0; i < ins.getNumOperands(); i++) {
         Value *op = ins.getOperand(i);
         if (acp.find(op) != acp.end()) {
@@ -148,8 +148,8 @@ void CopyPropagation::propagateCopies(BasicBlock &bb, ACPTable &acp)
       }
     } else if (isa<LoadInst>(iptr)) {
       Value *src = ins.getOperand(0);
-      ins.print(errs());
-      errs() << " src: " << src << " dest: " << (Value*)iptr << "\n";
+      // ins.print(errs());
+      // errs() << " src: " << src << " dest: " << (Value*)iptr << "\n";
       if (acp.find(src) != acp.end()) {
         // if src is in acp as a dest, store the src of that dest
         acp[(Value *)iptr] = acp[src];
